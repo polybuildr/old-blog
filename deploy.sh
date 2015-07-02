1ababa6b5f8434001e241cd38bcbe640657434c1
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2015-07-02 18:33:25 +0300

    Add auto-deploying stuff

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+HASH=$(git rev-parse --short HEAD)
+git clone --branch=gh-pages https://github.com/polybuildr/blog.git blog
+petroglyph --regenerate
+cd blog
+if [ -n "$(git status --porcelain)" ]; then
+    git config user.name "Travis CI"
+    git config user.email travis@travis-ci.org
+    git push https://${GH_TOKEN}@github.com/polybuildr/blog.git
+else
+    echo "No changes to push."
+fi
